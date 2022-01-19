2567b295f902456329cddddc6ce1c04c541d098a
janwillembuist <j.buist@fugro.com>
2022-01-19 20:21:05 +0100

    Changed to matplotlib graphs

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from io import BytesIO
 import streamlit as st
-import numpy as np
-from obspy import read as seg2read
+import matplotlib.pyplot as plt
+import obspy
+plt.rcParams['lines.linewidth'] = 1
 
 def parse_seg_file(file):
-    return seg2read(file)
+    return obspy.read(file)
+
+def plot_traces(stream: obspy.Stream):
+    p_fig, p_axs = plt.subplots(2, 1)
+    s_fig, s_axs = plt.subplots(2, 1)
+
+    p_axs[0].plot(stream[0].data)
+    p_axs[1].plot(stream[1].data)
+
+    s_axs[0].plot(stream[2].data)
+    s_axs[0].plot(stream[4].data)
+ 
+    s_axs[1].plot(stream[3].data)
+    s_axs[1].plot(stream[5].data)
+
+    return p_fig, s_fig
 
 def main():
     st.title('SEG2 Reader')
@@ -13,8 +28,10 @@ def main():
 
     if file is not None:
         stream = parse_seg_file(file)
-        for trace in stream:
-            st.line_chart(data=trace.data)
+        p_plot, s_plot = plot_traces(stream)
+
+        st.pyplot(p_plot)
+        st.pyplot(s_plot)
 
 if __name__ == '__main__':
     main()
