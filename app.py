4593e6fab283ee0b7d0ff1ca610f9c15eb3567bb
janwillembuist <j.buist@fugro.com>
2022-01-19 19:06:38 +0100

    Very basic seg plotter

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-import sys
-print(sys.version)
+from io import BytesIO
+import streamlit as st
+import numpy as np
+from obspy import read as seg2read
+
+def parse_seg_file(file):
+    return seg2read(file)
+
+def main():
+    st.title('SEG2 Reader')
+
+    file = st.file_uploader('Upload SEG2 file')
+
+    if file is not None:
+        stream = parse_seg_file(file)
+        for trace in stream:
+            st.line_chart(data=trace.data)
+
+if __name__ == '__main__':
+    main()
